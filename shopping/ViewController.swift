80415e099db5576cf3e1543dfb5559ffa665ca24
Pin Huang <justmailmason@gmail.com>
2020-08-19 11:39:57 +0800

    segment control not working

--- shopping/ViewController.swift
+++ shopping/ViewController.swift
@@ -5,15 +5,35 @@
 //  Created by Pincheng Huang on 2020/8/18.
 //
 
-import UIKit
+import UIKit // 匯入工具 UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet var containerViews: [UIView]!
+    
+    
+    
+override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        containerViews[0].isHidden = false
+        containerViews[1].isHidden = true
+        
     }
 
+    @IBAction func selectingCategory(_ sender: UISegmentedControl) {
+        
+        containerViews.forEach {
+           $0.isHidden = true
+        }
+        containerViews[sender.selectedSegmentIndex].isHidden = false
+     }
+    
+    
+    
+    
+    }
+    
 
-}
 
